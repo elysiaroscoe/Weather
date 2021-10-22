519b236e12fbf2874060a57476a3bb8ba7bbd716
Roscoe <elysiaroscoe@gmail.com>
2021-10-21 17:23:27 -0700

    initial

---
+++ script.js
@@ -0,0 +1,9 @@
+function alertBox(){
+    var element = document.querySelector("#city");
+    alert("Loading Weather Report...");
+}
+
+function removeAlert(){
+    var element = document.querySelector("#removeme");
+    element.remove();
+}
